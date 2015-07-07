b921563a8af106d18eef5f27ca3d1f88997f0219
zhujun <zhujun@1872.net>
2015-07-07 16:54:44 +0800

    完成新增收货地址界面

--- education/Section/HomePage/生活服务/AddReceiveAddressViewController.m
+++ education/Section/HomePage/生活服务/AddReceiveAddressViewController.m
@@ -8,7 +8,11 @@
 
 #import "AddReceiveAddressViewController.h"
 
-@interface AddReceiveAddressViewController ()
+@interface AddReceiveAddressViewController () {
+    BOOL addFlag;
+}
+@property (weak, nonatomic) IBOutlet UIButton *saveBtn;
+@property (weak, nonatomic) IBOutlet UIImageView *checkImageView;
 
 @end
 
@@ -18,12 +22,31 @@
     [super viewDidLoad];
     self.title = @"新增收货地址";
     
+    addFlag = NO;
+    
+    _saveBtn.layer.cornerRadius = 5.0f;
+    
     self.navigationItem.leftBarButtonItem = [Tools getNavBarItem:self clickAction:@selector(back)];
 }
 
 #pragma mark - Custom Method
 - (void)back {
     [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (IBAction)saveBtn:(id)sender {
+    
+}
+
+- (IBAction)setAdd:(id)sender {
+    if (addFlag) {
+        [_checkImageView setImage:[UIImage imageNamed:@"uncheckBtn"]];
+        addFlag = NO;
+    }
+    else {
+        [_checkImageView setImage:[UIImage imageNamed:@"checkBtn"]];
+        addFlag = YES;
+    }
 }
 
 - (void)didReceiveMemoryWarning {
