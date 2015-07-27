4301d1952bfd58e95a0335f429a4ffa868824c3b
zhujun <zhujun@1872.net>
2015-07-27 10:24:11 +0800

    添加启动页

---
+++ education/Section/GuidePage/GuidePageViewController.h
@@ -0,0 +1,17 @@
+//
+//  GuidePageViewController.h
+//  gaoqiutong
+//
+//  Created by zhu jun on 15-1-20.
+//  Copyright (c) 2015年 sportsexp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GuidePageViewController : UIViewController{
+    NSMutableArray *slideImages;
+    UIPageControl *_page;
+}
+
+
+@end
